2f1c5aec341d2207a79551b98bc111b2313e469f
Ben Gros <133671510+BenGros@users.noreply.github.com>
2023-06-07 09:59:53 -0400

    Update code.py

--- spaceships/code.py
+++ spaceships/code.py
@@ -2,6 +2,7 @@ import openai
 import os
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+# this method gets an answer from chatGPT
 def get_completion(prompt, model = "gpt-3.5-turbo"):
   messages = [{"role": "user", "content": prompt)]
   response = openai.ChatCompletion.create(
